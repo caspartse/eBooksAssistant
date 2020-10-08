8d55c2c8c4284a4f477b330a73fc244665179650
CasparTse <caspartse@users.noreply.github.com>
2020-10-08 10:03:10 +0800

    version 0.5.1

--- eBooksAssistant.user.js
+++ eBooksAssistant.user.js
@@ -2,9 +2,9 @@
 // @name         eBooks Assistant
 // @name:zh-CN   豆瓣读书助手
 // @namespace    https://github.com/caspartse/eBooksAssistant
-// @version      0.5.0
+// @version      0.5.1
 // @description  eBooks Assistant for douban.com
-// @description:zh-cn 为豆瓣读书页面添加亚马逊Kindle、图灵社区、喜马拉雅等链接
+// @description:zh-CN 为豆瓣读书页面添加亚马逊Kindle、图灵社区、喜马拉雅等链接
 // @author       Caspar Tse
 // @license      MIT License
 // @supportURL   https://github.com/caspartse/eBooksAssistant
@@ -33,9 +33,9 @@
                     var partnerTemplate = '';
                     if ($(".online-partner").length) {
                         partnerTemplate = '<div class="online-read-or-audio"> <a class="impression_track_mod_buyinfo" target="_blank" href="{templateUrl}"> <img src="https://s1.ax1x.com/2020/10/05/0JbHKI.jpg" width="16" height="16"> <span>Kindle</span> </a> </div>';
-                        $('.online-type[data-ebssistant="read"]').append(partnerTemplate.replace("{templateUrl}", amazonUrl));
+                        $('.online-type[data-ebassistant="read"]').append(partnerTemplate.replace("{templateUrl}", amazonUrl));
                     } else {
-                        partnerTemplate = '<div class="online-partner"> <div class="online-type" data-ebssistant="read"> <span>在线试读：</span> <div class="online-read-or-audio"> <a class="impression_track_mod_buyinfo" target="_blank" href="{templateUrl}" one-link-mark="yes"> <img src="https://s1.ax1x.com/2020/10/05/0JbHKI.jpg" width="16" height="16"> <span>Kindle</span> </a> </div></div> </div>';
+                        partnerTemplate = '<div class="online-partner"> <div class="online-type" data-ebassistant="read"> <span>在线试读：</span> <div class="online-read-or-audio"> <a class="impression_track_mod_buyinfo" target="_blank" href="{templateUrl}" one-link-mark="yes"> <img src="https://s1.ax1x.com/2020/10/05/0JbHKI.jpg" width="16" height="16"> <span>Kindle</span> </a> </div></div> </div>';
                         $("#link-report").after(partnerTemplate.replace("{templateUrl}", amazonUrl));
                     }
                     var regexAmazonPrice = /<span class="a-offscreen">￥([0-9\.]+)<\/span>/gi;
@@ -73,9 +73,9 @@
                     var partnerTemplate = '';
                     if ($(".online-partner").length) {
                         partnerTemplate = '<div class="online-read-or-audio"> <a class="impression_track_mod_buyinfo" target="_blank" href="{templateUrl}"> <img src="https://s1.ax1x.com/2020/10/06/0UsLZj.png" width="16" height="16"> <span>图灵社区</span> </a> </div>';
-                        $('.online-type[data-ebssistant="read"]').append(partnerTemplate.replace("{templateUrl}", turingUrl));
+                        $('.online-type[data-ebassistant="read"]').append(partnerTemplate.replace("{templateUrl}", turingUrl));
                     } else {
-                        partnerTemplate = '<div class="online-partner"> <div class="online-type" data-ebssistant="read"> <span>在线试读：</span> <div class="online-read-or-audio"> <a class="impression_track_mod_buyinfo" target="_blank" href="{templateUrl}" one-link-mark="yes"> <img src="https://s1.ax1x.com/2020/10/06/0UsLZj.png" width="16" height="16"> <span>图灵社区</span> </a> </div></div> </div>';
+                        partnerTemplate = '<div class="online-partner"> <div class="online-type" data-ebassistant="read"> <span>在线试读：</span> <div class="online-read-or-audio"> <a class="impression_track_mod_buyinfo" target="_blank" href="{templateUrl}" one-link-mark="yes"> <img src="https://s1.ax1x.com/2020/10/06/0UsLZj.png" width="16" height="16"> <span>图灵社区</span> </a> </div></div> </div>';
                         $("#link-report").after(partnerTemplate.replace("{templateUrl}", turingUrl));
                     }
                     var buyItemTemplate = '<li> <div class="cell price-btn-wrapper"> <div class="vendor-name"> <a target="_blank" href="{templateUrl}"> <span style="color:#418FDE;"><img src="https://s1.ax1x.com/2020/10/06/0UsLZj.png" style="border-radius: 50%; box-shadow: 0 0 1px 0 rgba(0,0,0,0.6);" width="16" height="16" border="0">&nbsp;图灵社区</span> </a> </div> <div class="cell impression_track_mod_buyinfo"> <div class="cell price-wrapper"> <a target="_blank" href="{templateUrl}"> <span class="buylink-price "> {templatePrice}元 </span> </a> </div> <div class="cell"> <a target="_blank" href="{templateUrl}" class="buy-book-btn e-book-btn"> <span>购买电子书</span> </a> </div> </div> </div> </li>';
@@ -89,10 +89,10 @@
         return;
     }
 
-    function queryXimalaya(title) {
+    function queryXimalaya(title, author) {
         GM_xmlhttpRequest({
             method: "GET",
-            url: "http://127.0.0.1:8081/ximalaya?title=" + title,
+            url: "http://127.0.0.1:8081/ximalaya?title=" + title + "&author=" + author,
             headers: {
                 'User-agent': window.navigator.userAgent,
             },
@@ -104,12 +104,12 @@
                     var partnerTemplate = '';
                     if ($(".online-partner .online-type").length == 2) {
                         partnerTemplate = '<div class="online-read-or-audio"> <a class="impression_track_mod_buyinfo" target="_blank" href="{templateUrl}"> <img src="https://s1.ax1x.com/2020/10/07/0UbEuj.png" width="16" height="16"> <span>喜马拉雅</span> </a> </div>';
-                        $('.online-type[data-ebssistant="audio"]').append(partnerTemplate.replace("{templateUrl}", ximalayaUrl));
+                        $('.online-type[data-ebassistant="audio"]').append(partnerTemplate.replace("{templateUrl}", ximalayaUrl));
                     } else if ($(".online-partner .online-type").length == 1) {
-                        partnerTemplate = '<div class="online-type" data-ebssistant="audio"> <span>在线试听：</span> <div class="online-read-or-audio"> <a class="impression_track_mod_buyinfo" target="_blank" href="{templateUrl}" one-link-mark="yes"> <img src="https://s1.ax1x.com/2020/10/07/0UbEuj.png" width="16" height="16"> <span>喜马拉雅</span> </a> </div></div>';
-                        $('.online-type[data-ebssistant="read"]').after(partnerTemplate.replace("{templateUrl}", ximalayaUrl));
+                        partnerTemplate = '<div class="online-type" data-ebassistant="audio"> <span>在线试听：</span> <div class="online-read-or-audio"> <a class="impression_track_mod_buyinfo" target="_blank" href="{templateUrl}" one-link-mark="yes"> <img src="https://s1.ax1x.com/2020/10/07/0UbEuj.png" width="16" height="16"> <span>喜马拉雅</span> </a> </div></div>';
+                        $('.online-type[data-ebassistant="read"]').after(partnerTemplate.replace("{templateUrl}", ximalayaUrl));
                     } else {
-                        partnerTemplate = '<div class="online-partner"> <div class="online-type" data-ebssistant="audio"> <span>在线试听：</span> <div class="online-read-or-audio"> <a class="impression_track_mod_buyinfo" target="_blank" href="{templateUrl}" one-link-mark="yes"> <img src="https://s1.ax1x.com/2020/10/07/0UbEuj.png" width="16" height="16"> <span>喜马拉雅</span> </a> </div></div> </div>';
+                        partnerTemplate = '<div class="online-partner"> <div class="online-type" data-ebassistant="audio"> <span>在线试听：</span> <div class="online-read-or-audio"> <a class="impression_track_mod_buyinfo" target="_blank" href="{templateUrl}" one-link-mark="yes"> <img src="https://s1.ax1x.com/2020/10/07/0UbEuj.png" width="16" height="16"> <span>喜马拉雅</span> </a> </div></div> </div>';
                         $("#link-report").after(partnerTemplate.replace("{templateUrl}", ximalayaUrl));
                     }
                 }
@@ -119,19 +119,35 @@
         return;
     }
 
-    var regexIsbn = /<span class="pl">ISBN:<\/span>\s*(\d+)\s*/g;
-    var isbn = regexIsbn.exec($("#info").html())[1];
-    console.log(isbn);
-    var title = $("#wrapper h1 span").text().trim();
-    console.log(title);
     try {
-        $(".online-partner .online-type:nth-child(1)").attr("data-ebssistant", "read");
-        $(".online-partner .online-type:nth-child(2)").attr("data-ebssistant", "audio");
+        $(".online-partner .online-type:nth-child(1)").attr("data-ebassistant", "read");
+        $(".online-partner .online-type:nth-child(2)").attr("data-ebassistant", "audio");
     } catch(e) {
         console.log(e);
     }
+    var newStyle = `<style type="text/css" media="screen">
+.online-partner{flex-wrap:wrap;padding-top:5px;padding-bottom:5px}.online-type{flex-wrap:wrap}.online-read-or-audio{margin-top:5px;margin-bottom:5px}.online-partner .online-type:nth-child(1){margin-right:20px}.online-partner .online-type:nth-child(2){padding-left:0}
+</style>`;
+    $("#content").append(newStyle);
+
+    var regexlinkedData = /<script type="application\/ld\+json">([\s\S]+?)<\/script>/g;
+    var linkedData = regexlinkedData.exec(document.documentElement.innerHTML)[1].trim();
+    linkedData = JSON.parse(linkedData);
+    console.log(linkedData);
+    var isbn = linkedData.isbn;
+    console.log(isbn);
+    var title = linkedData.name;
+    console.log(title);
+    var authorStr = "";
+    for (var i=0, j=linkedData.author.length; i<j; i++) {
+        authorStr += linkedData.author[i].name + " " ;
+    }
+    var author = authorStr;
+    console.log(author);
+
     queryAmazon(title, isbn);
     queryTuring(isbn);
-    queryXimalaya(title);
+    queryXimalaya(title, author);
+
     return;
 })();
